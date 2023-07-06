772a931d58be83e7eb58b37d06ab7973d2dd2dac
kar61614 <112227050+kar61614@users.noreply.github.com>
2023-07-06 11:30:59 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song ="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristx = 0;
+rightWristY = 0;
+
 
 function preload(){
     song = loadSound("music.mp3");
@@ -11,6 +16,13 @@ function setup(){
  video = createCapture(VIDEO);
  video.hide();
 
+ poseNet = ml5.poseNet(video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+
+}
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
 }
 
 function draw(){
@@ -19,4 +31,21 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftwrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY =" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rigtWristY);
+         
+    }
 }
